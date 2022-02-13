1dceae5a1af0662d35e81cf602231e1223b1468f
Mengxue1104 <59936757+Mengxue1104@users.noreply.github.com>
2022-02-13 14:08:43 -0700

    env bug

--- src/components/db/firebaseConfig.jsx
+++ src/components/db/firebaseConfig.jsx
@@ -6,13 +6,13 @@ import {getDatabase} from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "Process.env.STOREFRONT_FIREBASE_API_KEY",
-  authDomain: "Process.env.STOREFRONT_FIREBASE_AUTH_DOMAIN",
-  databaseURL: "Process.env.STOREFRONT_FIREBASE_DATABASE_URL",
-  projectId: "Process.env.STOREFRONT_FIREBASE_PROJECT_ID",
-  storageBucket: "Process.env.STOREFRONT_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "Process.env.STOREFRONT_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "Process.env.Process.env.STOREFRONT_FIREBASE_APP_ID"
+  apiKey: process.env.STOREFRONT_FIREBASE_API_KEY,
+  authDomain: process.env.STOREFRONT_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.STOREFRONT_FIREBASE_DATABASE_URL,
+  projectId: process.env.STOREFRONT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.STOREFRONT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.STOREFRONT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.STOREFRONT_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
