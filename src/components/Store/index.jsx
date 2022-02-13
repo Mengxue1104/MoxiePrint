28585d20ec3a32b3c48419b4c953ba8a6cd0f3bb
Mengxue1104 <59936757+Mengxue1104@users.noreply.github.com>
2022-02-13 14:22:34 -0700

    env update

--- src/components/Store/index.jsx
+++ src/components/Store/index.jsx
@@ -139,7 +139,7 @@ export default function Store() {
                                     </Col>
                                 }
 
-                                //return <></>
+                                return <></>
                             })}
                         </Row>
                     </Container>
